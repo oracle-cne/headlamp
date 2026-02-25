e6768407e1e891d8f6fb980289aa540c50facea2
Murali Annamneni <murali.annamneni@oracle.com>
2026-02-25 12:09:21 +0000

    Reload homepage after login to show app-catalog
    Due to a regression, app-catalog sidebar entry is not showingup uponlogin.
    So, reload the page as a temp. fix.
    Also, listing installed helm releases had a regression, so, fixed it.
    
    Signed-off-by: Murali Annamneni <murali.annamneni@oracle.com>

--- frontend/src/components/account/Auth.tsx
+++ frontend/src/components/account/Auth.tsx
@@ -57,6 +57,7 @@ export default function AuthToken() {
             })
           );
         }
+        window.location.reload();
       } else {
         setToken('');
         setShowError(true);
